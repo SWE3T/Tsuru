759e34e5fe0c32c329f7579d0face618b7fb677a
Guilherme <guilherme.devon@hotmail.com>
2021-09-12 19:05:57 -0300

    Loading model

--- src/sketch.js
+++ src/sketch.js
@@ -2,34 +2,38 @@ let img;
 let myFont;
 
 function preload() {
-    myFont = loadFont('src/NotoSansJP-Black.otf');
+    myFont = loadFont('src/NotoSansJP-Medium.otf');
+    tsuru = loadModel('src/tsuru.obj');
 }
 
 function setup() {
     createCanvas(500, 500, WEBGL);
     img = loadImage('https://swe3t.github.io/Just-wait-a-sec/src/queroVenderMinhaArte.png');
     textFont(myFont);
-    textSize(width / 3);
+    textSize(height / 4);
     textAlign(CENTER, CENTER);
 }
 
 function draw() {
-    background(0);
-
-    text('吉', 0, 0);
+    if (frameCount % 3 == 0) {
+        text('吉', random(-width, width), random(-height, height));
+    }
 
 
     let locX = mouseX;
     let locY = mouseY;
 
     ambientLight(255, 255, 255);
-    //pointLight(255, 255, 255, locX, locY, 100);
+    pointLight(255, 255, 255, locX, locY, 100);
 
     push();
     rotateZ(frameCount * 0.001);
     rotateX(locX * 0.005);
     rotateY(locY * 0.005);
-    box(200);
+
+    scale(50);
+
+    model(tsuru);
     pop();
 }
 
